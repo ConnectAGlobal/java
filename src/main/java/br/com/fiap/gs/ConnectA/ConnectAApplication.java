69bde2bffbe06c2f8c71afd8d119ab000e2f6fdc
Vinicius de Oliveira Coutinho <562663381@PMB.local>
2025-11-10 15:46:15 -0300

    First Commit

---
+++ src/main/java/br/com/fiap/gs/ConnectA/ConnectAApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gs.ConnectA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectAApplication.class, args);
+	}
+
+}
